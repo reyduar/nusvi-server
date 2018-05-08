821bc6d3830c3f85cc64a7f80049c05a99abe7f1
ariel.duarte <arielpy@gmail.com>
2018-05-08 01:05:28 -0300

    Agregado CRUD de imagenes

--- app.js
+++ app.js
@@ -15,20 +15,34 @@ app.use(bodyParser.json())
 var appRoutes = require("./routes/appRoute");
 var usuarioRoutes = require("./routes/usuarioRoute");
 var loginRoutes = require("./routes/loginRoute");
+var hospitalRoutes = require('./routes/hospitalRoute');
+var medicoRoutes = require('./routes/medicoRoute');
+var busquedaRoutes = require('./routes/busquedaRoute');
+var uploadRoutes = require('./routes/uploadRoute');
+var imagenesRoutes = require('./routes/imagenesRoute');
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://admin:password@ds163699.mlab.com:63699/node-api-mdb', (err, res) => {
-
     if (err) throw err;
-
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
-
 });
 
+// Server index config
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
+
 // Rutas
-app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
+app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/', appRoutes);
+
 
 
 // Escuchar peticiones
